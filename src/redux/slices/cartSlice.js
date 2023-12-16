41a30301f3c0527f00e47a18a25ed9358bed0b08
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-16 16:54:22 +0700

    solve problem

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,9 +1,36 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState: {
+//     data: JSON.parse(localStorage.getItem('cart')|| []),
+//   },
+//   reducers: {
+//     addToCart: (state, action) => {
+//       const itemInCart = state.data.find(
+//         (item) => item.id === action.payload.id
+//       );
+//       if (itemInCart) {
+//         itemInCart.qty++;
+//       } else {
+//         state.data.push(action.payload);
+//       }
+//     },
+//   },
+// });
+
+// export const { addToCart } = cartSlice.actions;
+// export default cartSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
+
+// Get cart data from localStorage, or set an empty array if it doesn't exist or is invalid
+const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart")) || [];
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    data: JSON.parse(localStorage.getItem('cart')|| []),
+    data: cartFromLocalStorage,
   },
   reducers: {
     addToCart: (state, action) => {
@@ -15,9 +42,12 @@ const cartSlice = createSlice({
       } else {
         state.data.push(action.payload);
       }
+      // Update localStorage with the modified cart data
+      localStorage.setItem("cart", JSON.stringify(state.data));
     },
   },
 });
 
 export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
